22aaee3f897def9e14f772e78960dce41bc00b51
arthur lin <arthurliinn@gmail.com>
2019-02-27 21:18:38 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //loading
 $(window).load(function () {
     $('#loadingPage').fadeOut();
-    $('#webBody').animate({ opacity: 1, height: "auto" }, 500);
+    $('#webBody').animate({ opacity: 1, height: "auto" }, 2000);
     $('body').css("overflow-y", "auto");
 
 });

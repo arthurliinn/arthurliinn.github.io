cf754a954e8f619bbd8c7ade94bb52c1c4fd77d8
arthur lin <arthurliinn@gmail.com>
2019-03-04 22:33:45 +0800

    update

--- js/main.js
+++ js/main.js
@@ -3,9 +3,32 @@ $(window).load(function () {
     $('#loadingPage').fadeOut();
     $('#webBody').animate({ opacity: 1, height: "auto" }, 2000);
     $('body').css("overflow-y", "auto");
+    
 
 });
 
 //scroll show
+$(window).scroll(function(){
+    $('.hideBox').each(function(i){
+        var window_scrollHeight =  $(window).scrollTop() + $(window).height() ;
+        var object_scrollHeight = $(this).offset().top + $(this).outerHeight()-500;
 
-$(window).scrollTop()
+        if(window_scrollHeight > object_scrollHeight){
+            $(this).animate({opacity:1},1000);
+            
+            
+        }
+
+        
+    });
+
+    /*$('.level').each(function(i){
+        var window_scrollHeight =  $(window).scrollTop() + $(window).height() ;
+        var level_scrollHeight = $('.level').offset().top + $('.level').outerHeight();
+
+        if(window_scrollHeight > level_scrollHeight){
+            
+        }
+    });*/
+});
+
